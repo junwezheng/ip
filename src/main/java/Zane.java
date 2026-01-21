dd86f6f3257252fa03a35a210f4576a1b518986b
Junwei <zhengjunwei01@gmail.com>
2026-01-21 22:27:57 +0800

    Added support for list

--- src/main/java/Zane.java
+++ src/main/java/Zane.java
@@ -1,25 +1,47 @@
 import java.util.Scanner;
 
 public class Zane {
+    public static final String LINE = "____________________________________________________________";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int count = 0;
 
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
         System.out.println("Hello! I'm Zane");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
 
         while (true) {
-            String userInput = scanner.nextLine();
+            String userInput = scanner.nextLine().trim();
             if (userInput.equals("bye")) {
-                System.out.println("____________________________________________________________");
+                System.out.println(LINE);
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println(LINE);
                 break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
+
+            if (userInput.equals("list")) {
+                printList(list, count);
+            } else {
+                list[count] = userInput;
+                count++;
+                printMessage( "added: " + userInput);
+            }
         }
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(LINE);
+        System.out.println(message);
+        System.out.println(LINE);
+    }
+
+    public static void printList(String[] list, int count) {
+        System.out.println(LINE);
+        for (int i = 0; i < count; i++) {
+            System.out.println((i + 1) + ". " + list[i]);
+        }
+        System.out.println(LINE);
     }
 }
