080b1e6bc89a5dc228c6ba634e7876d933821ce7
Junwei <zhengjunwei01@gmail.com>
2026-02-06 11:22:21 +0800

    Add initial structure for GUI according to the javaFx tutorial

---
+++ src/main/java/zane/ui/MainWindow.java
@@ -0,0 +1,47 @@
+package zane.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Zane zane;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.jpg"));
+    private Image zaneImage = new Image(this.getClass().getResourceAsStream("/images/Zane.jpg"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setZane(Zane z) {
+        zane = z;
+    }
+
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = zane.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, zaneImage)
+        );
+        userInput.clear();
+    }
+}
